98adf297934f017e99e241bc7f82b0011dafd75e
Thomas <tfedoraw@live.fr>
2022-06-14 08:35:15 +0200

    ajout du del

--- src/main/java/project/motus/Joueur.java
+++ src/main/java/project/motus/Joueur.java
@@ -2,8 +2,15 @@ package project.motus;
 
 public class Joueur {
 	private String name;
+	private int points;
 	
 	public Joueur(String name) {
 		this.name = name;
+		this.points = 0;
+	}
+	
+	public Joueur(String name, int points) {
+		this.name = name;
+		this.points = points;
 	}
 }
